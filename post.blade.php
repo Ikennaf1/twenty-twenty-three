a505483a05fd028d62439a0b2c839d917e260cf8
Ikennaf1 <iiikbros202@gmail.com>
2023-10-07 23:58:18 +0100

    Initial commit

---
+++ post.blade.php
@@ -0,0 +1,19 @@
+
+
+{{-- {!! getPostHead($post) !!} --}}
+{{-- <div></div> --}}
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+        <link rel="stylesheet" href="/assets/css/style.css">
+    </head>
+
+    <body>
+        <div class="font-bold">jfkjdbfd</div>
+        {{ $post }}
+    </body>
+</html>
