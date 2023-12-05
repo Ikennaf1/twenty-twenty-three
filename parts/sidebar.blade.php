7a82d38e6ce325d6b24fa33aa8c2b4ad5d7e24ad
Ikennaf1 <iiikbros202@gmail.com>
2023-12-05 12:48:47 +0100

    Widgets loading working

--- parts/sidebar.blade.php
+++ parts/sidebar.blade.php
@@ -7,15 +7,14 @@
         @if (!empty($widgets))
             @foreach ($widgets as $widget)
                 <div class="flex flex-col gap-1">
-                    <div class="sidebar-widget-title font-bold border-l border-l-4 border-l-primary-color pl-2 py-1 bg-gray-200">
+                    <div class="sidebar-widget-title">
                         {{ getWidgetTitle($widget) }}
                     </div>
-                    <div class="sidebar-widget-body pl-3">
+                    <div class="sidebar-widget-body">
                         {!! getWidgetBody($widget) !!}
                     </div>
                 </div>
             @endforeach
         @endif
     </div>
-    
 </div>
