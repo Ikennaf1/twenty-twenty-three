f6889327bf0a3a79b37b4d4a01112a54b75d7018
Ikennaf1 <iiikbros202@gmail.com>
2024-03-16 00:24:18 +0100

    worked on the sidebar and footer

--- parts/sidebar.blade.php
+++ parts/sidebar.blade.php
@@ -5,16 +5,18 @@
 <div class="sidebar-container">
     <div class="sticky top-40">
         @if (!empty($widgets))
+        <div class="flex flex-col gap-4">
             @foreach ($widgets as $widget)
-                <div class="flex flex-col gap-1">
+                <div class="flex flex-col gap-2">
                     <div class="widget-title">
-                        {{ getWidgetTitle('right-sidebar', 'Recent posts') }}
+                        {{ getWidgetTitle('right-sidebar', $widget->name) }}
                     </div>
                     <div class="widget-body">
                         {!! getWidgetBody($widget) !!}
                     </div>
                 </div>
             @endforeach
+        </div>
         @endif
     </div>
 </div>
